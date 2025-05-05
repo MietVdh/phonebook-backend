a01b232881369128ac7e78e6aa009fac2f12057c
MietVdh <miet.vanderheyden@gmail.com>
2025-05-05 14:52:06 -0400

    Added static page

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express()
 
 const morgan = require('morgan')
 
+
 app.use(express.json())
 morgan.token('req_body', function getRequestBody (req) {
     return JSON.stringify(req.body)
   })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req_body'))
-
+app.use(express.static('dist'))
 
 
 let persons = [
