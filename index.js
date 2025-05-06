bf8118f4467ce5aedecfe0b66f5fbd21ee767100
MietVdh <miet.vanderheyden@gmail.com>
2025-05-06 12:38:17 -0400

    Completed exercise 3.13

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require('express')
-const app = express()
-
+require('dotenv').config()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+    Person.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+} else if (process.argv.length == 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({ name, number })
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+const app = express()
 
 app.use(express.json())
 morgan.token('req_body', function getRequestBody (req) {
@@ -12,32 +44,9 @@ morgan.token('req_body', function getRequestBody (req) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req_body'))
 app.use(express.static('dist'))
 
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
+    
     const person = persons.find(person => person.id === id)
     if (person) {
         response.json(person)
@@ -47,7 +56,11 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+        mongoose.connection.close()
+    })
+    
 })
 
 app.get('/info', (request, response) => {
@@ -97,7 +110,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
